06f3b21ba0aaac710c93ec5e1cddc615776f0c8b
VideoCarp <66365570+VideoCarp@users.noreply.github.com>
2020-09-21 12:58:28 +1000

    Update main.py

--- main.py
+++ main.py
@@ -1,16 +1,17 @@
 # Imports
-import time
-from tkinter import *
+import time; import webbrowser; import subprocess
+from tkinter import *; import os
 from tkinter.scrolledtext import ScrolledText
 from tkinter import messagebox as tkmsg
 from tkinter import filedialog
+
 
 
 # Initialize Window
 window = Tk()
 
 window.title("Notepad--")
-window.iconbitmap("icon.ico")
+#window.iconbitmap("icon.ico")
 
 # Set up
 window.configure(bg="#222")
@@ -40,9 +41,35 @@ def openDialog():
   # Write down into text box.
   msgbox("Opened", "File opened successfully.")
 
-
+# Run As Python
 def _runModule():
-  msgbox('Output:', eval(text_box.get(1.0, END)))
+  msgbox('Ran as Python', "Successfully ran file.")
+  eval(text_box.get(1.0, END))
+
+
+# Run As Lua
+def runLua():
+  msgbox("Ran as Lua.", "Successfully ran file.")
+  luaGen = open("minimal_luaexec.lua", "+w")
+  luaGen.write("-- Minimal Editor created this file" + text_box.get(1.0, END))
+  subprocess.check_output(['lua', '-l', 'demo', '-', 'minimal_luaexec.lua'])
+  luaGen.close()
+
+# HTML
+def html_page():
+  msgbox("HTML", "Ran HTML file successfully.")
+  min_html = open("minimal_html.html", "+w")
+  min_html.write("<!-- minimal_editor -->\n" + text_box.get(1.0, END))
+  min_html.close()
+  webbrowser.open(f"file://{os.path.realpath('minimal_html.html')}")
+
+# JavaScript
+def js_page():
+  msgbox("JavaScript", "Ran JavaScript as webpage successfully. Node.js is currently unsupported, manually run.")
+  min_js = open("minimal_js.html", "+w")
+  min_js.write(f"<script>{text_box.get(1.0, END)}</script>")
+  min_js.close()
+  webbrowser.open(f"file://{os.path.realpath('minimal_js.html')}")
 
 # The Input Box
 text_box = ScrolledText(background="#222", foreground="#aaa") # Main Text Box
@@ -59,15 +86,22 @@ def saveBox():
   # Confirm
   msgbox("Saved", "File has been saved successfully.")
 
+# Menus
+_menu = Menu()
+fileMenu = Menu(_menu, tearoff=0)
+fileMenu.add_command(label="Open", command=openDialog)
+fileMenu.add_command(label="Save", command=saveBox)
 
-# Buttons
-saveButton = Button(background="#222", foreground="#aaa", text="Save", command = saveBox)
-openButton = Button(background="#222", foreground="#aaa", text="Open", command = openDialog)
-evalButton = Button(background="#222", foreground="#aaa", text="Run Script", command = _runModule)
+runMenu = Menu(_menu, tearoff=0)
+runMenu.add_command(label="Run Script as Python", command=_runModule)
+runMenu.add_command(label="Run Script as Lua", command=runLua)
+runMenu.add_command(label="Run File as Webpage (HTML)", command=html_page)
+runMenu.add_command(label="Run as JavaScript Page", command=js_page)
+
+_menu.add_cascade(label="File", menu=fileMenu)
+_menu.add_cascade(label="Run", menu=runMenu)
 
 # West, East, North, South
-saveButton.grid(column=1, row=1, sticky=W)
-openButton.grid(column=1, row=1, sticky=W, padx=75)
-evalButton.grid(column=1, row=1, sticky=W, padx=160)
+window.config(menu=_menu)
 text_box.grid(column=1, row=2)
 window.mainloop()
